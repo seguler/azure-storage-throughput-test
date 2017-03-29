16c1f2775271d87c5510e0be5798e355b79c7062
seguler <seguler@microsoft.com>
2017-03-29 16:15:02 -0700

    removed sudo

--- setup_env.sh
+++ setup_env.sh
@@ -4,20 +4,21 @@
 echo "Installing .NET Core"
 
 
-sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ yakkety main" > /etc/apt/sources.list.d/dotnetdev.list'
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
-apt-get update
+sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ yakkety main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
+sudo apt-get update
 
-apt-get install dotnet-dev-1.0.1 -y
+sudo apt-get install dotnet-dev-1.0.1 -y
 
 echo "Cloning the sample"
-cd /home
+cd /home/$1
 git clone https://github.com/Azure-Samples/storage-blob-coreclr-linux-getting-started-w-data-movement-library
 
 cd storage-blob-coreclr-linux-getting-started-w-data-movement-library
-chown -R $1 .
-sudo -u $1 dotnet restore
-sudo -u $1 dotnet build
+
+echo "Restoring the nuget packages and building"
+dotnet restore
+dotnet build
 
 echo "Creating dummy files from dev/urandom"
 mkdir test
@@ -26,10 +27,5 @@ dd if=/dev/urandom of=samplefile bs=1G count=2
 
 echo "Resetting the permissions"
 cd ..
-chown -R $1 .
 
 echo "done"
-
-
-
-
