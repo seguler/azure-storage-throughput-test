ef92e1ec4e9555608227abe8bb87321b281f029f
seguler <seguler@microsoft.com>
2017-03-29 00:02:26 -0700

    fix variable name in azuredeploy.json

--- setup_env.sh
+++ setup_env.sh
@@ -10,9 +10,12 @@ sudo apt-get -y update
 
 sudo apt-get -y install dotnet-dev-1.0.1
 
+echo "Cloning the sample"
 git clone https://github.com/Azure-Samples/storage-blob-coreclr-linux-getting-started-w-data-movement-library
 
-mkdir test
+cd storage-blob-coreclr-linux-getting-started-w-data-movement-library
+dotnet restore
+dotnet build
 
 
 
