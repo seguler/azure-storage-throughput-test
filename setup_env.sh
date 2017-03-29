8491af75c5d9e3094da70a7521508de048b20d29
seguler <seguler@microsoft.com>
2017-03-28 23:55:41 -0700

    setup script

--- setup_env.sh
+++ setup_env.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
 # install dotnet core
-echo "test"
+echo "Installing .NET Core"
+
+
+sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ yakkety main" > /etc/apt/sources.list.d/dotnetdev.list'
+sudo apt-key -y adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
+sudo apt-get -y update
+
+sudo apt-get -y install dotnet-dev-1.0.1
+
+git clone https://github.com/Azure-Samples/storage-blob-coreclr-linux-getting-started-w-data-movement-library
+
+mkdir test
+
+
+
+
+
