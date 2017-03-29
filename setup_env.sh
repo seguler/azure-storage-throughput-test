d3a5b3f93e9e42c0c4dcfb03363bdfbd98db3c54
seguler <seguler@microsoft.com>
2017-03-29 10:56:07 -0700

    create dummy file

--- setup_env.sh
+++ setup_env.sh
@@ -4,11 +4,11 @@
 echo "Installing .NET Core"
 
 
-sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ yakkety main" > /etc/apt/sources.list.d/dotnetdev.list'
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
-sudo apt-get update
+sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ yakkety main" > /etc/apt/sources.list.d/dotnetdev.list'
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
+apt-get update
 
-sudo apt-get install dotnet-dev-1.0.1 -y
+apt-get install dotnet-dev-1.0.1 -y
 
 echo "Cloning the sample"
 cd /home
@@ -18,6 +18,16 @@ cd storage-blob-coreclr-linux-getting-started-w-data-movement-library
 dotnet restore
 dotnet build
 
+echo "Creating dummy files from dev/urandom"
+mkdir test
+cd test
+dd if=/dev/urandom of=samplefile bs=1G count=2
+
+echo "Resetting the permissions"
+cd ..
+chmod -R 755 .
+
+echo "done"
 
 
 
