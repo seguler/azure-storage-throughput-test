0b3d006c473a6ccae2de72c23353da3ac25f189a
Sercan Guler [MSFT] <seguler@microsoft.com>
2018-03-30 22:51:29 -0700

    Update setup_env.sh

--- setup_env.sh
+++ setup_env.sh
@@ -2,11 +2,12 @@
 
 echo "###Installing .NET Core"
 
-sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ yakkety main" > /etc/apt/sources.list.d/dotnetdev.list'
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-get update
 
-sudo apt-get install dotnet-dev-1.0.1 -y
+sudo apt-get install dotnet-sdk-2.1.4 -y
 
 echo "###Cloning the sample"
 cd /home/$1
